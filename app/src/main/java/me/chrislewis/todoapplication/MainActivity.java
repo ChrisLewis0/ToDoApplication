e8a4f924b04d9d534a57c7092884510b740f69ee
Christopher Lewis <cwlewis@fb.com>
2018-06-27 17:40:08 -0700

    Dark Background

--- app/src/main/java/me/chrislewis/todoapplication/MainActivity.java
+++ app/src/main/java/me/chrislewis/todoapplication/MainActivity.java
@@ -1,14 +1,17 @@
 package me.chrislewis.todoapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.commons.io.FileUtils;
@@ -41,7 +44,22 @@ public class MainActivity extends AppCompatActivity {
         // initialize the items list
         readItems();
         // initialize the adapter using the items list
-        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                // Get the Item from ListView
+                View view = super.getView(position, convertView, parent);
+
+                // Initialize a TextView for ListView each Item
+                TextView tv = (TextView) view.findViewById(android.R.id.text1);
+
+                // Set the text color of TextView (ListView Item)
+                tv.setTextColor(Color.WHITE);
+
+                // Generate ListView Item using TextView
+                return view;
+            }
+        };
         // wire the adapter to the view
         lvItems.setAdapter(itemsAdapter);
 
